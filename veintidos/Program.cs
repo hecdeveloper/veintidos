1f46a0232cd6585eeb767365ddf666a85ac4edd6
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 16:35:22 -0500

    autos

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -8,7 +8,24 @@ namespace Switch
         static string? clave;
         static void Main(string[] args)
         {
-      
+            int autos = 5;
+
+            switch (autos)
+            {
+              case 0:
+                Console.WriteLine("No paga impuestos");
+                break;
+            case 1:
+                Console.WriteLine("Paga $15");
+                break;
+            case 2:
+                Console.WriteLine("Paga $35");
+                break;
+            default:
+                Console.WriteLine("Paga $15 por cada auto");
+                break;
+            } 
+            Console.Read();
         }
 
     }
