d46d906159755da9f448855f9c8452181dbaed17
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 16:17:32 -0500

    switch

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace veintidos
+namespace Switch
 {
     class Program
     {
@@ -8,39 +8,9 @@ namespace veintidos
         static string? clave;
         static void Main(string[] args)
         {
-            Registro();
-            Ingreso();
-            Console.Read();
+      
         }
-        public static void Registro()
-        {
-            Console.WriteLine("Ingrese su nombre");
-            nombreDeUsuario = Console.ReadLine();
-            Console.WriteLine("Ingrese su clave");
-            clave = Console.ReadLine();
-            Console.WriteLine("Registro exitoso");
-            Console.WriteLine("--------------------");
-        }
-        public static void Ingreso()
-        {
-            Console.WriteLine("Ingrese su nombre");
-            if (nombreDeUsuario == Console.ReadLine())
-            {
-                Console.WriteLine("Ingrese su clave");
-                if (clave == Console.ReadLine())
-                {
-                    Console.WriteLine("Ingreso exitoso");
-                }
-                else
-                {
-                    Console.WriteLine("Clave incorrecta");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Usuario incorrecto");
-            }
-        }
+
     }
 }
 
