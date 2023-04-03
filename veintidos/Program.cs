80c7defc08e5df80ca39c83488b35dd145e6e6a0
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 14:53:41 -0500

    void

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -6,26 +6,12 @@ namespace veintidos
     {
         static void Main(string[] args)
         {
-            bool esAdmin = false;
-            bool estaregistrado = true;
-            string nombreDeUsuario = "";
-            Console.WriteLine("Por favor, ingrese su nombre de usuario");
-            nombreDeUsuario = Console.ReadLine();
-            if(estaregistrado && nombreDeUsuario != "" && nombreDeUsuario.Equals("Admin"))
-            {
-                Console.WriteLine("Hola usuario registrado");
-             
-                Console.WriteLine("Hola " + nombreDeUsuario);
-                    
-                 Console.WriteLine("Hola Admin"); 
-            }
-            if(esAdmin || estaregistrado)
-            {
-                Console.WriteLine("Estas Logueado");
-            }
-            Console.Read();
+            
         }
-     
+     public static void Registro()
+     {
+        Console.WriteLine("Ingrese su nombre");
+     }
     }
 }
 
