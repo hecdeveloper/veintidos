612eab698126b6023fc7de0e0d4a364bcbd45c3f
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 12:37:37 -0500

    test

---
+++ veintidos/Anidado.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace veintidos
+{
+    class Program                    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la temperatura actual");
+
+            string  temperatura = Console.ReadLine();
+            int numTemperatura;
+            int numero;
+
+            if(int.TryParse(temperatura, out numero))
+            {
+                numTemperatura = numero;
+            }
+            else
+            {
+                numTemperatura = 0;
+                Console.WriteLine("El valor ingresado no es valido");
+            }
+            if (numTemperatura < 20)
+            {
+                Console.Write("Abrigate!");
+            }
+            else if(numTemperatura == 20)
+            {
+                Console.WriteLine("Vistete Comodo");
+            }
+            else if (numTemperatura == 30)
+            {
+                Console.WriteLine("Hace 30 grados que calor");
+            }
+            else
+            {
+                Console.WriteLine("Usa ropa liviana");
+            }
+            Console.Read();
+
+        }
+    }
+}
+
