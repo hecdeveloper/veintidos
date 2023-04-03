f983f1bab54fa7b4f3ccf2103b05ddc14a5e01e0
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 12:37:53 -0500

    test

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace veintidos
+{
+    class Anidado                    
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
 
