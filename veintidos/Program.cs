e399c30dd05b5ef80560d77208913830db7e911c
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 16:15:55 -0500

    login

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -2,36 +2,45 @@
 
 namespace veintidos
 {
-    class Program                    
+    class Program
     {
         static string? nombreDeUsuario;
         static string? clave;
         static void Main(string[] args)
         {
-            
+            Registro();
+            Ingreso();
+            Console.Read();
         }
-     public static void Registro()
-     {
-        Console.WriteLine("Ingrese su nombre");
-        nombreDeUsuario = Console.ReadLine();
-        Console.WriteLine("Ingrese su clave");
-        clave = Console.ReadLine();
-        Console.WriteLine("Registro exitoso");
-        Console.WriteLine("--------------------");
-     }
-     public static void Ingreso()
-     {
-        Console.WriteLine("Ingrese su nombre");
-        if(nombreDeUsuario == Console.ReadLine())
+        public static void Registro()
         {
+            Console.WriteLine("Ingrese su nombre");
+            nombreDeUsuario = Console.ReadLine();
             Console.WriteLine("Ingrese su clave");
-            if(clave == Console.ReadLine())
+            clave = Console.ReadLine();
+            Console.WriteLine("Registro exitoso");
+            Console.WriteLine("--------------------");
+        }
+        public static void Ingreso()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            if (nombreDeUsuario == Console.ReadLine())
             {
-                Console.WriteLine("Ingreso exitoso");
+                Console.WriteLine("Ingrese su clave");
+                if (clave == Console.ReadLine())
+                {
+                    Console.WriteLine("Ingreso exitoso");
+                }
+                else
+                {
+                    Console.WriteLine("Clave incorrecta");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usuario incorrecto");
             }
         }
-
-     }
     }
 }
 
