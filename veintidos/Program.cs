b1f24994e1ccd4dcef47f421f067971e141ef2c5
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 13:08:04 -0500

    update

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -6,7 +6,22 @@ namespace veintidos
     {
         static void Main(string[] args)
         {
+            bool esAdmin = false;
+            bool estaregistrado = true;
+            string nombreDeUsuario = "";
+            if(estaregistrado)
+            {
+                Console.WriteLine("Hola usuario registrado");
+                if(nombreDeUsuario != "")
+                {
+                    Console.WriteLine("Hola" + nombreDeUsuario);
+                    if (nombreDeUsuario.Equals("Admin"));
+                    {
+                        Console.WriteLine("Hola Admin");
+                    }
+                }
 
+            }
         }
     }
 }
