b5165d9826e8f5bb5837f56f633ea46bd6b6d3c8
Hector Ibarra <hibarra@imaginuity.com>
2023-04-03 13:31:35 -0500

    ifs anidados

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -2,27 +2,31 @@
 
 namespace veintidos
 {
-    class Anidado                    
+    class Program                    
     {
         static void Main(string[] args)
         {
             bool esAdmin = false;
             bool estaregistrado = true;
             string nombreDeUsuario = "";
+            Console.WriteLine("Por favor, ingrese su nombre de usuario");
+            nombreDeUsuario = Console.ReadLine();
             if(estaregistrado)
             {
                 Console.WriteLine("Hola usuario registrado");
                 if(nombreDeUsuario != "")
                 {
-                    Console.WriteLine("Hola" + nombreDeUsuario);
-                    if (nombreDeUsuario.Equals("Admin"));
+                    Console.WriteLine("Hola " + nombreDeUsuario);
+                    if (nombreDeUsuario.Equals("Admin"))
                     {
                         Console.WriteLine("Hola Admin");
                     }
                 }
 
             }
+            Console.Read();
         }
+     
     }
 }
 
