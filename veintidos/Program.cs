bddb6d03e536cc30f34ad4f26499c9a15dcd42e4
Hector Ibarra <hibarra@imaginuity.com>
2023-04-10 18:31:33 -0500

    props

--- veintidos/Program.cs
+++ veintidos/Program.cs
@@ -9,15 +9,7 @@ namespace  Clases
         static void Main(string[] args)
         {
 
-            Humano juan = new Humano("Juan");
-            Humano juana = new Humano("Juana", "Garay", "verde");
-            Humano milena = new Humano("Juana", "Millera");
-            Humano antonio = new Humano("Antonio", "Cama", 7, "Azul");
-
-            juan.presentarme();
-            juana.presentarme();
-            milena.presentarme();
-            antonio.presentarme();
+   
 
             Console.Read();
 
